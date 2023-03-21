156775c80bb663ad84ba20a1fa3ea81f27acbc7b
Dustin Getz <dustin.getz@gmail.com>
2023-03-21 14:04:40 -0400

    Auto saved by Logseq

--- logseq/custom.js
+++ logseq/custom.js
@@ -8,49 +8,15 @@
 (() => {
     class QueryPortal extends HTMLElement {
         constructor() { super(); }
-
         connectedCallback() {
-            this.innerHTML = `<b>Hello World</b>`
-
-            // read attribute:
             // this.getAttribute('category')
             // e.g. <query-portal category="something" />
-
-            // read text content
-            // this.textContent
-            //
-            // const query = `[:find (pull ?b [*])
-            //             :where
-            //             [?b :block/marker ?marker]
-            //             [(contains? #{"NOW" "LATER" "TODO" "DOING"} ?marker)]]`;
-            //
-            // ;(async () => {
-            //     try {
-            //         // run query and get blocks
-            //         const blocks = await this.fetch(query);
-            //         // render them
-            //         this.render(blocks);
-            //     } catch (e) {
-            //         this.innerHTML = e;
-            //     }
-            // })()
+            // this.textContent -- read text content
+            this.innerHTML = `<b>Hello World</b>`
         }
-
-        // async fetch(query) {
-        //     let ret = await logseq.DB.datascriptQuery(query);
-        //     return ret?.flat() // not groupped by page
-        // }
-
-        // render(blocks) {
-        //     const items = blocks.map(function (block) {
-        //         //return `<li>${JSON.stringify(block, 2, 2)}</li>`
-        //         return `<li>${block.content}</li>`
-        //     });
-        //
-        //     const template = `<ul>${items.join('')}</ul>`
-        //     this.innerHTML = template
-        // }
     }
+
+    window.dustin = 42
 
     window.customElements.define('query-portal', QueryPortal)
 })();
